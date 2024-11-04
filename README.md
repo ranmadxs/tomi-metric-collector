064634bf1b3e95636293900c22fc7a70b87b810e
Edgar Sanchez <ranmakun@MacBook-Pro-de-Edgar.local>
2024-11-04 15:48:08 -0300

    0.1.3

--- README.md
+++ README.md
@@ -1,19 +1,32 @@
-```sh {"id":"01HJQ7F9RXZBJJ4YEQA7Q49GYF"}
+# Tomi Metric Collector
+
+## Iniciar la aplicación
+
+```sh
 poetry run python tomi_metrics/app.py
+```
 
+o utilizando Gunicorn:
 
+```sh
 poetry run gunicorn -w 4 -b 0.0.0.0:8000 tomi_metrics.app:app --reload
 ```
+
+## Ejemplos de solicitudes
+
+### Enviar métrica localmente
 
 ```sh
 curl -X POST http://localhost:5000/metrics \
      -H "Content-Type: application/json" \
      -d '{
-           "metric": "cpu_usage",
-           "points": [[1700749200, 0.5]],
-           "tags": ["host:server1", "environment:production"]
+           "metric": "tomi.metric.collector.test2.counter",
+           "points": [[1730745173, 2]],
+           "tags": ["host:localhost", "environment:develop"]
          }'
 ```
+
+### Enviar log localmente
 
 ```sh
 curl -X POST http://localhost:5000/log \
@@ -22,11 +35,14 @@ curl -X POST http://localhost:5000/log \
            "message": "An error occurred",
            "level": "error",
            "service": "test",
+           "ddsource": "python",
+           "hostname": "localhost",
+           "tags": ["env:production"],
            "date": "2024-10-28T12:34:56"
          }'
 ```
 
-### tomi-metric-collector-production.up.railway.app
+### Enviar métrica a producción
 
 ```sh
 curl -X POST https://tomi-metric-collector-production.up.railway.app/metrics \
@@ -38,6 +54,8 @@ curl -X POST https://tomi-metric-collector-production.up.railway.app/metrics \
          }'
 ```
 
+### Enviar log a producción
+
 ```sh
 curl -X POST https://tomi-metric-collector-production.up.railway.app/log \
      -H "Content-Type: application/json" \
@@ -45,19 +63,29 @@ curl -X POST https://tomi-metric-collector-production.up.railway.app/log \
            "message": "An error occurred",
            "level": "error",
            "service": "test",
+           "ddsource": "python",
+           "hostname": "production-server",
+           "tags": ["env:production"],
            "date": "2024-10-28T12:34:56"
          }'
 ```
 
+### Verificar respuesta HTTP sin mostrar salida
+
 ```sh
-curl -o /dev/null -s -w "%{http_code}\n" -X POST http://tomi-metric-collector-production.up.railway.app/log \
+curl -o /dev/null -s -w "%{http_code}\n" -X POST https://tomi-metric-collector-production.up.railway.app/log \
      -H "Content-Type: application/json" \
      -d '{
            "message": "System initialized",
            "level": "info",
-           "service": "test"
+           "service": "test",
+           "ddsource": "python",
+           "hostname": "production-server",
+           "tags": ["env:production"]
          }'
 ```
+
+### Ejemplo de log adicional a producción
 
 ```sh
 curl -X POST https://tomi-metric-collector-production.up.railway.app/log \
@@ -65,6 +93,9 @@ curl -X POST https://tomi-metric-collector-production.up.railway.app/log \
      -d '{
            "message": "System started",
            "service": "test",
-           "level": "info"
+           "level": "info",
+           "ddsource": "python",
+           "hostname": "production-server",
+           "tags": ["env:production"]
          }'
 ```
--- tomi_metrics/app.py
+++ tomi_metrics/app.py
@@ -15,9 +15,10 @@ metrics = []
 logs = []
 
 # Configuración de DataDog y MongoDB desde variables de entorno
-DATADOG_API_URL = "https://api.datadoghq.com/api/v1/series"
+DATADOG_API_URL = "https://api.datadoghq.com/api/v1/series?api_key="
 DATADOG_API_KEY = os.getenv("DATADOG_API_KEY")  # Lee la API Key de DataDog desde variables de entorno
 MONGO_URI = os.getenv("MONGODB_URI")  # Lee la URI de MongoDB desde variables de entorno
+ENABLE_SEND_LOG_TO_DATADOG = False  # Flag para habilitar/deshabilitar el envío de logs a DataDog
 
 # Conexión a MongoDB
 mongo_client = MongoClient(MONGO_URI)
@@ -29,31 +30,54 @@ def send_metric_to_datadog(metric_name, tags, points):
     Función para enviar métricas a la API de DataDog con soporte para puntos específicos.
     """
     headers = {
-        "Content-Type": "application/json",
-        "DD-API-KEY": DATADOG_API_KEY
+        "Content-Type": "application/json"
     }
     payload = {
         "series": [
             {
                 "metric": metric_name,
-                "points": points,  # Utiliza el parámetro points que incluye el timestamp y valor
+                "points": points,
                 "tags": tags
             }
         ]
     }
 
-    response = requests.post(DATADOG_API_URL, json=payload, headers=headers)
+    response = requests.post(DATADOG_API_URL + DATADOG_API_KEY, json=payload, headers=headers)
+    print(f"{response.status_code} {response.json()}")
     return response.status_code, response.json()
 
-def save_log_to_mongodb(message, level, log_date, service):
+from datetime import datetime
+
+def save_log_to_mongodb(message, level, log_date, service, ddsource, hostname, tags):
     """
-    Función para almacenar logs en MongoDB.
+    Función para almacenar logs en MongoDB con la estructura completa y con fecha de creación automática.
     """
+    # Obtener la fecha y hora actual en formato datetime para MongoDB
+    created_at = datetime.utcnow()
+    
+    # Convertir log_date a datetime si es una cadena en el formato especificado
+    if isinstance(log_date, str):
+        try:
+            log_date = datetime.strptime(log_date, '%Y-%m-%dT%H:%M:%S')
+        except ValueError:
+            print("Error: log_date no tiene el formato correcto. Usando fecha actual.")
+            log_date = created_at  # Usa la fecha actual si el formato no coincide
+    elif log_date is None:
+        log_date = created_at  # Usa la fecha actual si no se proporciona log_date
+
+    # Procesar tags: si es una cadena, dividirla en una lista
+    if isinstance(tags, str):
+        tags = tags.split(',')  # Divide los tags en una lista si están en una cadena separada por comas
+
     log = {
         "message": message,
         "level": level,
         "date": log_date,
-        "service": service
+        "service": service,
+        "ddsource": ddsource,
+        "hostname": hostname,
+        "tags": tags,           # Almacenar los tags como una lista de strings
+        "created_at": created_at
     }
     try:
         logs_collection.insert_one(log)
@@ -61,6 +85,30 @@ def save_log_to_mongodb(message, level, log_date, service):
     except Exception as e:
         print(f"Error al insertar el log en MongoDB: {e}")
         raise
+
+
+
+def send_log_to_datadog(log_entry):
+    """
+    Función para enviar logs a DataDog solo si el flag ENABLE_SEND_LOG_TO_DATADOG está habilitado.
+    """
+    if not ENABLE_SEND_LOG_TO_DATADOG:
+        return  # Salir si el envío está deshabilitado
+
+    try:
+        headers = {
+            "Content-Type": "application/json",
+            "DD-API-KEY": DATADOG_API_KEY
+        }
+        datadog_response = requests.post(
+            "https://http-intake.logs.datadoghq.com/v1/input",
+            json=log_entry,
+            headers=headers
+        )
+        if datadog_response.status_code != 200:
+            print(f"Error al enviar el log a DataDog: Status {datadog_response.status_code}")
+    except Exception as e:
+        print(f"Error al enviar el log a DataDog: {e}")
 
 @app.route('/')
 def hello_world():
@@ -88,26 +136,45 @@ def save_metric():
         "tags": tags
     }
     metrics.append(metric)
-    return jsonify({"message": "Metric saved ok"}), 201  # Devuelve un mensaje de éxito en el cuerpo
+    return jsonify({"message": "Metric saved ok"}), 201
 
 @app.route('/log', methods=['POST'])
 def save_log():
     data = request.json
     message = data.get('message')
-    level = data.get('level')
-    service = data.get('service')
+    level = data.get('level', 'info')
+    service = data.get('service', 'my-service')
+    ddsource = data.get('ddsource', 'python')
+    hostname = data.get('hostname', os.getenv("HOSTNAME", "localhost"))
+    tags = data.get('tags', [])
     log_date = data.get('date', datetime.utcnow().isoformat())
 
     # Validación de campos obligatorios
-    if not message or not level or not service:
+    if not message or not service:
         return '', 400
 
+    # Crear el log en el formato esperado por DataDog
+    log_entry = {
+        "message": message,
+        "ddsource": ddsource,
+        "service": service,
+        "hostname": hostname,
+        "status": level,
+        "tags": tags,
+        "date": log_date
+    }
+
+    # Guardar en MongoDB con el nuevo formato
     try:
-        # Guardar log en MongoDB
-        save_log_to_mongodb(message, level, log_date, service)
-        return jsonify({"message": "Log saved ok"}), 201  # Devuelve un mensaje de éxito en el cuerpo
+        save_log_to_mongodb(message, level, log_date, service, ddsource, hostname, tags)
     except Exception as e:
-        return '', 500  # Devuelve solo el código de error sin cuerpo en caso de fallo
+        print(f"Error al insertar el log en MongoDB: {e}")
+        return '', 500
+
+    # Enviar log a DataDog
+    send_log_to_datadog(log_entry)
+
+    return jsonify({"message": "Log saved ok"}), 201
 
 if __name__ == '__main__':
     app.run(debug=True)
